1fdd90f6530e99100213c3d38adaa6ba247d4967
Ed2002 <ed49516@gmail.com>
2021-07-31 21:02:16 -0300

    Moviment Script
    Moviment Script

---
+++ hero of generation/Assets/Scripts/Player/Moviment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviment : MonoBehaviour
+{
+
+    [SerializeField] float Speed = 4f;
+    Rigidbody2D controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMoviment();
+    }
+
+    void UpdateMoviment()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * Speed;
+        controller.velocity = new Vector2(moveBy, controller.velocity.y);
+    }
+}
